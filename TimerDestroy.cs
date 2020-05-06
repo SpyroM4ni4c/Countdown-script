7f95ef1ccf69bb481ec7768b2d3e5f475e1bf23f
Archorr <64887716+SpyroM4ni4c@users.noreply.github.com>
2020-05-06 10:06:14 +0300

    Add files via upload

---
+++ TimerDestroy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerDestroy : MonoBehaviour
+{
+	public float interval;
+
+
+
+
+
+	void Start()
+	{
+		Destroy(gameObject, interval);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
